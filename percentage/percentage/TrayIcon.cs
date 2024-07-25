ec1ff9a4ee1a2687bb422ad6fdaea389aa46a2f6
filipkruza <74458014+filipkruza@users.noreply.github.com>
2024-07-25 20:19:12 +0200

    moved solution, and update to .net 4.7.2

--- percentage/percentage/TrayIcon.cs
+++ percentage/percentage/TrayIcon.cs
@@ -7,7 +7,7 @@ namespace percentage
 {
     class TrayIcon
     {
-        [DllImport("user32.dll", CharSet=CharSet.Auto)]
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
         static extern bool DestroyIcon(IntPtr handle);
 
         private const int fontSize = 18;
@@ -24,65 +24,85 @@ namespace percentage
 
             contextMenu.MenuItems.AddRange(new MenuItem[] { menuItem });
 
-            menuItem.Click += new System.EventHandler(MenuItemClick);
+            menuItem.Click += new System.EventHandler(MenuExitClick);
             menuItem.Index = 0;
-            menuItem.Text = "E&xit";
+            menuItem.Text = "Exit";
 
             notifyIcon.ContextMenu = contextMenu;
             notifyIcon.Visible = true;
 
             Timer timer = new Timer();
             timer.Interval = 1000;
-            timer.Tick += new EventHandler(TimerTick);
+            timer.Tick += new EventHandler(Update);
             timer.Start();
         }
 
         private Bitmap GetTextBitmap(String text, Font font, Color fontColor)
         {
-            SizeF imageSize = GetStringImageSize(text, font);
-            Bitmap bitmap = new Bitmap((int)imageSize.Width, (int)imageSize.Height);
-            using (Graphics graphics = Graphics.FromImage(bitmap))
+            (int,int) MeasureStringSize()
             {
-                graphics.Clear(Color.FromArgb(0, 0, 0, 0));
-                using (Brush brush = new SolidBrush(fontColor))
-                {
-                    graphics.DrawString(text, font, brush, 0, 0);
-                    graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
-                    graphics.Save();
-                }
+                SizeF size;
+                using (Image image = new Bitmap(1, 1))
+                    using (Graphics graphics = Graphics.FromImage(image))
+                        size = graphics.MeasureString(text, font);
+                return ((int)size.Width, (int)size.Height);
             }
-            return bitmap;
+            Bitmap DrawBitmap(int width, int height)
+            {
+                Bitmap bitmap = new Bitmap(width, height);
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    graphics.Clear(Color.FromArgb(0, 0, 0, 0));
+                    using (Brush brush = new SolidBrush(fontColor))
+                    {
+                        graphics.DrawString(text, font, brush, 0, 0);
+                        graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+                        graphics.Save();
+                    }
+                }
+                return bitmap;
+            }
+            var imageSize = MeasureStringSize();
+            return DrawBitmap(imageSize.Item1, imageSize.Item2);
         }
 
-        private static SizeF GetStringImageSize(string text, Font font)
-        {
-            using (Image image = new Bitmap(1, 1))
-            using (Graphics graphics = Graphics.FromImage(image))
-                return graphics.MeasureString(text, font);
-        }
-
-        private void MenuItemClick(object sender, EventArgs e)
+        private void MenuExitClick(object sender, EventArgs e)
         {
             notifyIcon.Visible = false;
             notifyIcon.Dispose();
             Application.Exit();
         }
 
-        private void TimerTick(object sender, EventArgs e)
+        private void Update(object sender, EventArgs e)
         {
-            PowerStatus powerStatus = SystemInformation.PowerStatus;
-            String percentage = (powerStatus.BatteryLifePercent * 100).ToString();
-            bool isCharging = SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Online;
-            String bitmapText = isCharging ? percentage + "*" : percentage;
-            using (Bitmap bitmap = new Bitmap(GetTextBitmap(bitmapText, new Font(font, fontSize), Color.White)))
+            (String, String) GetPowerStrings()
             {
-                System.IntPtr intPtr = bitmap.GetHicon();
+                PowerStatus powerStatus = SystemInformation.PowerStatus;
+                String percentage = (powerStatus.BatteryLifePercent * 100).ToString();
+                bool isCharging = SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Online;
+                String tooltipText = percentage + "%" + (isCharging ? " Charging" : "");
+                String bitmapText = percentage;
+                if (percentage == "100")
+                    bitmapText = "F";
+                return (bitmapText, tooltipText);
+            }
+
+            Bitmap CreateBitmap(String BitmapText, Color BitmapColor)
+            {
+                return new Bitmap(GetTextBitmap(BitmapText, new Font(font, fontSize), BitmapColor));
+            }
+
+            (String, String) texts = GetPowerStrings();
+            Color fontColor = Color.White;
+            using (Bitmap bitmap = CreateBitmap(texts.Item1,fontColor))
+            {
+                IntPtr intPtr = bitmap.GetHicon();
                 try
                 {
                     using (Icon icon = Icon.FromHandle(intPtr))
                     {
                         notifyIcon.Icon = icon;
-                        String toolTipText = percentage + "%" + (isCharging ? " Charging" : "");
+                        String toolTipText = texts.Item2;
                         notifyIcon.Text = toolTipText;
                     }
                 }
